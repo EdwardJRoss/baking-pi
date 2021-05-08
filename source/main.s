1e5374aa47a9bea392e860fc05d87336fffaac2c
Edward Ross <edward@skeptric.com>
2021-05-08 22:21:10 +1000

    OK02: Flash the OK light about 2 times/s

--- source/main.s
+++ source/main.s
@@ -11,10 +11,29 @@ _start:
         lsl r1,#18
         str r1,[r0,#4]
 
-        # Turn off GPIO Pin 16
+        # Store adress of GPIO 16 in r1
         mov r1,#1
         lsl r1,#16
+loop$:
+        # Turn off pin (i.e. turn light on)
         str r1,[r0,#40]
 
-loop$:
+        # Wait a little while
+        mov r2,#0x3F0000
+wait1$:
+        sub r2,#1
+        cmp r2,#0
+        bne wait1$
+
+        # Turn on pin (i.e. turn light off)
+        str r1,[r0,#28]
+
+        # Wait a little while
+        mov r2,#0x3F0000
+wait2$:
+        sub r2,#1
+        cmp r2,#0
+        bne wait2$
+
+        # Back to the start
         b loop$
