7a04f2682df02beb7a431b500e034b20ab1e76d3
Edward Ross <edward@skeptric.com>
2021-05-10 21:26:24 +1000

    OK05: Implement SOS Morse Code Signal

--- source/main.s
+++ source/main.s
@@ -16,35 +16,41 @@ main:
         .unreq pinNum
         .unreq pinFunc
 
+        # Load in the pattern
+        ptrn .req r4
+        ldr ptrn,=pattern
+        ldr ptrn,[ptrn]
+        seq .req r5
+        mov seq,#0
+
 loop$:
-        # Turn off pin (i.e. turn light on)
-        pinNum .req r0
+        # Set the pinVal to the pattern at seq
         pinVal .req r1
-        mov pinNum,#16
-        mov pinVal,#0
-        bl SetGpio
-        .unreq pinNum
-        .unreq pinVal
-
-
-        # Sleep for about 0.5s
-        # 5e5 = 5e3 * 1000 ~ 500 * 2^10
-        mov r0,#500
-        lsl r0,#10
-        bl Sleep
-
-        # Turn on pin (i.e. turn light off)
-        pinNum .req r0
-        pinVal .req r1
-        mov pinNum,#16
         mov pinVal,#1
+        lsl pinVal,seq
+        and pinVal,ptrn
+
+        # seq += 1 (mod 32)
+        add seq, seq, #1
+        and seq, #0b11111
+
+
+        # Set the pin
+        pinNum .req r0
+        mov pinNum,#16
         bl SetGpio
         .unreq pinNum
         .unreq pinVal
 
-        mov r0,#500
+        # Sleep for about 0.25s
+        mov r0,#250
         lsl r0,#10
         bl Sleep
 
         # Back to the start
         b loop$
+
+.section .data
+.align 2
+pattern:
+    .int 0b11111111101010100010001000101010
